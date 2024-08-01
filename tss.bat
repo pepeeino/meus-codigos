58b0fd7393bfbad23a188d0e1c56b68a23ad4f69
pepeeino <gutytelles@gmail.com>
2024-08-01 17:03:54 -0300

    pepeeino commit

--- tss.bat
+++ tss.bat
@@ -1,4 +1,4 @@
 ::start program
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Users\HP\Desktop\\IniciarRAMMap.ps1"
+start "" "C:\Program Files (x86)\RAMMap\RAMMap.exe"
 ::n use auto hot key, ele tem spy ware, n é so pq falaram q é pra tu baixar q tu tem q baixar
