93c3b36969661ff7757f98273df5a26937a3e114
pepeeino <gutytelles@gmail.com>
2024-08-01 16:58:12 -0300

    pepeeino commit

--- tss.bat
+++ tss.bat
@@ -1,3 +1,4 @@
 ::start program
 @echo off
-start "" "C:\Users\HP\Desktop\progamaçaõ\tss.ahk"
+powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Users\HP\Desktop\\IniciarRAMMap.ps1"
+::n use auto hot key, ele tem spy ware, n é so pq falaram q é pra tu baixar q tu tem q baixar
