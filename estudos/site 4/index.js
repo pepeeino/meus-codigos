2005c4e542e8a3c02ea222fa5bc32f999a6aefde
pepeeino <gutytelles@gmail.com>
2024-08-06 14:59:04 -0300

    pepeeino commits

---
+++ estudos/site 4/index.js
@@ -0,0 +1,7 @@
+document.getElementById("somar").addEventListener("click", function () {
+  var numero1 = parseFloat(document.getElementById("numero1").value);
+  var numero2 = parseFloat(document.getElementById("numero2").value);
+  var resultado = numero1 + numero2;
+
+  document.getElementById("resultado").textContent = "Resultado: " + resultado;
+});
