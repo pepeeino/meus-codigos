63fe996da6ee14af001f32e2f52ef5f18c8a0bcc
pepeeino <gutytelles@gmail.com>
2024-08-07 23:47:49 -0300

    pepeeino

--- estudos/site 4/index.js
+++ estudos/site 4/index.js
@@ -4,4 +4,22 @@ document.getElementById("somar").addEventListener("click", function () {
   var resultado = numero1 + numero2;
 
   document.getElementById("resultado").textContent = "Resultado: " + resultado;
+
+  document
+    .getElementById("resultado")
+    .setAttribute("data-resultado", resultado);
 });
+
+document
+  .getElementById("resultado")
+  .addEventListener("DOMSubtreeModified", function () {
+    var resultado = document
+      .getElementById("resultado")
+      .getAttribute("data-resultado");
+
+    if (resultado == "1234") {
+      alert("aqueles q partiram sempre estarão conosco");
+    } else {
+      alert("i love math");
+    }
+  });
