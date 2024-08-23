643de9d846f9fabaeb37842c51b58442f96f1d66
pepeeino <gutytelles@gmail.com>
2024-08-23 12:21:37 -0300

    pepeeino commit

--- projeto/requisição.js
+++ projeto/requisição.js
@@ -1,12 +1,12 @@
 function validate_site() {
   const input = document.getElementById("input").value;
-  const resultado = document.getElementById("resultado");
+  const output = document.getElementById("output");
 
-  fetch("input")
+  fetch(input)
     .then((response) => {
-      resultado.textContent = `Response: ${response.status}`;
+      output.textContent = `Response: ${response.status}`;
     })
     .catch((error) => {
-      resultado.textContent = `Error: ${error.message}`;
+      output.textContent = `Error: ${error.message}`;
     });
 }
